4927ac67a0db733badef7be9832285e7b1dd3385
sneha638 <babysnahe2021@gmail.com>
2023-01-06 13:49:05 +0530

    changes made

---
+++ src/Utils/Middleware/validatioMiddleware.js
@@ -0,0 +1,11 @@
+const validationMiddleware = Schema=> (req,res,next)=>{
+    const {error} = Schema.validate(req.body,{abortEarly:false})
+   if (error){
+    return res
+    .status(400)
+    .send(
+        error?.map(({message,path})=>({message,path}))
+    );
+   }
+   next();
+};
