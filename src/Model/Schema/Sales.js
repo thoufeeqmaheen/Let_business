965a2b5c835715da3237e904b116eb5b60e0934b
sneha638 <babysnahe2021@gmail.com>
2023-01-06 11:41:17 +0530

    changes made

---
+++ src/Model/Schema/Sales.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const sales = mongoose.Schema(
+    {
+        label: String,
+        delete:{ 
+            type : Boolean,
+            default : false,
+        },
+        productList:[SalesProduct],
+        user:{
+            type:mongoose.Types.ObjectId,
+            ref:"user",
+        },
+        shop:{
+            type:mongoose.Types.ObjectId,
+            ref:"shop",
+        },
+    
+    {
+        timestamps : true,
+    },
+}
+
+);
+module.exports = mongoose.model("sales",sales)
