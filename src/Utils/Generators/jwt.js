f77570a6ce930df49f2e0dcd6a39d79f163664ee
sneha638 <babysnahe2021@gmail.com>
2023-01-06 12:30:00 +0530

    changes made

---
+++ src/Utils/Generators/jwt.js
@@ -0,0 +1,24 @@
+const jwt = require ("jsonwebtoken");
+ const{JWT_SECRET} = reuire("../../Config/env");
+
+ const createToken = (id) => {
+   const token = jwt.sign(
+    {
+        id,
+    },
+    JWT_SECRET
+   );
+   return token;
+};
+
+const verifyToken =(token) => {
+    const decoded = jwt.verify(token,JWT_SECRET);
+    return decoded;
+
+};
+
+module.exports = {
+    createToken,
+    verifyToken,
+};
+
