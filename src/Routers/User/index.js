9afda082e696ee76649d3d8def64df768086eb2d
sneha638 <babysnahe2021@gmail.com>
2023-01-09 17:06:08 +0530

    changes made

--- src/Routers/User/index.js
+++ src/Routers/User/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const { getUserById } = require("../../Model/Shop");
 
 const router = express.Router();
 
+router.get("/",async(req,res)=>{
+    const result = await getUserById(req.user.id);
+    res.send(result);
+});
+
+router.put("/",async(req,res)=>{
+    const result = await updateUser(req.user.id,req.body);
+    res.send(result)
+});
+
+router.delete("/",async(req,res)=>{
+    const result = await deleteUser(req.user.id);
+    res.send(result)
+
+});
 
 module.exports = router;
