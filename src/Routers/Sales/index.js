61a49e59d2d8ad8ce5e8df19d45b5f5f6611c872
sneha638 <babysnahe2021@gmail.com>
2023-01-09 16:52:41 +0530

    changes made

---
+++ src/Routers/Sales/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/",async (req,res)=>{
+    const result = await createSale({
+        ...req.body
+
+    });
+    res.send(result);
+});
+
+
+
+router.get("/",async (req,res)=>{
+    const result = await getSale();
+    const data = result.map((d)=>({...d._doc,
+        price:d._doc.productsList.reduce(
+        (a,p)=>a+p.quantity*p.pricePerQuantuty,
+        0
+        ),
+    }));
+    res.send(data);
+});
+
+router.get("/:id",async (req,res)=>{
+     const result = await getSaleById(req.params.id);
+     const data ={
+        ...result._doc,
+            price:result.productsList.reduce(
+            (a,p)=>a+p.quantity*p.pricePerQuantuty,
+            0
+            ),
+        };
+        res.send(data);
+    });
+
+
+
+
+
+module.exports = router;
