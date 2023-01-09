27215f887705d76ac4a46193ae5c5182ecfe5ea2
sneha638 <babysnahe2021@gmail.com>
2023-01-09 12:00:46 +0530

    changes made

--- src/Routers/index.js
+++ src/Routers/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-   res.send("API router");
-});
-router.use("/shop",require("./Shop"))
+
+router.use("/auth",authMiddleware, require("./Auth"));
+router.use("/shop",authMiddleware,require("./Shop"));
+router.use("/user",authMiddleware,require("./User"));
+router.use("/sales",authMiddleware,require("./Sales"));
+router.use("/util",require("./Util"));
+
 
 module.exports = router;
