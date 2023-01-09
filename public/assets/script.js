60121828ef73d3681ca480ae2ead4ce060596f54
sneha638 <babysnahe2021@gmail.com>
2023-01-09 13:33:39 +0530

    changes made

--- public/assets/script.js
+++ public/assets/script.js
@@ -5,8 +5,43 @@ buttons.addEventListener("click",() =>{
 decodeURIComponent.getElementById("root").innerText = "clicked";
 
 });
+console.log(document);
+
 fetch("/api/v1/util/shop-list")
 .then(r=>r.json())
 .then(data=>{
-    console.log(data);
-})
+    const out=data
+    .map(
+        (shop)=>
+    <div>
+    <p>
+        shop:${shop.name}
+    </p>
+    <p>
+        Address:${shop.address}
+        </p>
+        <p>
+            phone:${shop.phone}
+        </p>
+        <h3>
+            Products
+        </h3>
+        <ul>
+            ${
+                shop.products.map(
+                    product=>`<li>${product.name}`
+                ).join("")
+            }
+            <li>
+                pdt1
+            </li>
+            <li>
+                pdt2
+            </li>
+        </ul>
+
+</div>
+    )
+.join("");
+document.getElementById("shop-list").innerHTML = out;
+});
