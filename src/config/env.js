396f83df2985a5b191f2a4a7c408563636326f59
sneha638 <babysnahe2021@gmail.com>
2023-01-10 12:19:13 +0530

    changes made

--- src/config/env.js
+++ src/config/env.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const API_ROUTER = process.env.API_ROUTER || "/api/v1";
 const CONNECTION_STRING = process.env.CONNECTION_STRING;
+const JWT_SECRET = process.env.JWT_SECRET||"JWT_SECRET";
+
 
 module.exports ={
     PORT,
