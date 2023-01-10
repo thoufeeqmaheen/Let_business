b6eb6ad5e02b7e19c59fc89cedd41233054d83ac
sneha638 <babysnahe2021@gmail.com>
2023-01-10 15:29:21 +0530

    changes made

--- src/Routers/Shop/index.js
+++ src/Routers/Shop/index.js
@@ -5,10 +5,16 @@ const { getShops,
     createShop,
     updateShop,
     deleteShop,
-    } = require("../../Model/Shop");
+    getProducts,
+    addProduct,
+    removeProduct,
+    updateProduct,
+ } = require("../../Model/Shop");
 
-router.post("/",async(req,res)=>{
-    const result = await createShop(req.body);
+
+
+router.get("/",async(req,res)=>{
+    const result = await getShops(req.user.id);
     res.send(result)
 });
 
@@ -16,14 +22,46 @@ router.get("/:id",async(req,res)=>{
     const result = await getShopById(req.params.id);
     res.send(result)
 });
+
+router.post("/",async(req,res)=>{
+    const result = await createShop({
+    ...req.body,
+    user:req.user.id,
+});
+    res.send(result)
+});
+
 router.put("/:id",async(req,res)=>{
     const result = await updateShop(req.params.id,req.body);
     res.send(result)
 });
+
 router.delete("/:id",async(req,res)=>{
     const result = await deleteShop(req.params.id);
     res.send(result)
 
+});
+
+router.get("/product/:id",async(req,res)=>{
+    const result = await getProducts(req.params.id);
+    res.send(result)
+});
+
+router.post("/product/:id",async(req,res)=>{
+    const result = await addProduct(req.params.id,req.body);
+    res.send(result)
+});
+
+router.put("/product/:id",async(req,res)=>{
+    const result = await updateProduct(req.params.id,req.body);
+    res.send(result)
+});
+
+router.delete("/product/:id",async(req,res)=>{
+    const result = await removeProduct(req.params.id,{
+        _id:req.body.id,
+    });
+    res.send(result);
 });
 
 module.exports = router;
